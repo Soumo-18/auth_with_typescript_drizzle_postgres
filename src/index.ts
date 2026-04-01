844e1d03414fc7875584a52e70888bec34100ec4
Soumo-18 <soumomandal7@gmail.com>
2026-04-01 17:12:23 +0530

    feat: set up HTTP server and application structure
    - Added main function to initialize and start an HTTP server on port 8080.
    - Created a new Express application in src/app/index.ts with a welcome route.
    - Updated tsconfig.json to include necessary libraries and types for Node.js.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('HELLO')
+import { createServer } from 'node:http'
+import { createApplication } from './app/index.js'
+
+async function main () {
+    try{
+        const server = createServer(createApplication())
+        const port :number=8080
+
+        server.listen(port,()=>{
+            console.log(`HTTP Server is running on ${port}`)
+        })
+
+    } catch(err){
+        console.log(`Error starting http server`)
+        throw err
+    }
+}
+
+main()
